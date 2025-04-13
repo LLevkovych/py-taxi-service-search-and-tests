ef2fc8b9a932471cf1ff93066d9a486a0faedfa1
Liubomyr Levkovych <liubomyrlevkovich@gmail.com>
2025-04-13 21:11:09 +0300

    Solution

--- taxi/tests.py
+++ taxi/tests.py
@@ -1,4 +1,4 @@
-from django.test.testcases import TestCase
+from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from taxi.models import Manufacturer, Car
